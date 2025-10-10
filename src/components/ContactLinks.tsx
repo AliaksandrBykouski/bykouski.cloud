f278036a77486c28d33ddf438daeda4d5a64275f
Aliaksandr <157883894+AliaksandrBykouski@users.noreply.github.com>
2025-10-11 01:11:08 +0200

    v1.0.0

--- src/components/ContactLinks.tsx
+++ src/components/ContactLinks.tsx
@@ -1,19 +1,20 @@
 import { Clock, Mail, MessageCircle } from "lucide-react";
+import Link from "next/link";
 
 export default function ContactLinks({
   params,
 }: {
   params: {
     email: string;
-    whatsapp: string;
+    telegram: string;
     responseTime: string;
     responseTimeText: string;
     writeMe: string;
   };
 }) {
   const contactInfo = {
-    email: "aliaksandr.bykouski@example.com",
-    whatsapp: "+420123456789",
+    email: "post@bykouskidigital.cz",
+    telegram: "Raulius7",
   };
 
   return (
@@ -22,22 +23,29 @@ export default function ContactLinks({
         <h3 className="text-lg font-semibold">{params.email}</h3>
         <div className="flex items-center gap-3">
           <Mail className="h-5 w-5 text-muted-foreground text-[var(--ring)]" />
-          <a href={`mailto:${contactInfo.email}`} className="text-primary hover:underline">
+          <Link
+            href={`mailto:${contactInfo.email}`}
+            className="text-primary hover:underline"
+            target="_blank"
+            rel="noreferrer"
+          >
             {contactInfo.email}
-          </a>
+          </Link>
         </div>
       </div>
 
       <div className="space-y-4">
-        <h3 className="text-lg font-semibold">WhatsApp</h3>
+        <h3 className="text-lg font-semibold">Telegram</h3>
         <div className="flex items-center gap-3">
           <MessageCircle className="h-5 w-5 text-muted-foreground text-[var(--ring)]" />
-          <a
-            href={`https://wa.me/${contactInfo.whatsapp}`}
+          <Link
+            href={`https://t.me/${contactInfo.telegram}`}
             className="text-primary hover:underline"
+            target="_blank"
+            rel="noreferrer"
           >
             {params.writeMe}
-          </a>
+          </Link>
         </div>
       </div>
 
@@ -45,7 +53,9 @@ export default function ContactLinks({
         <h3 className="text-lg font-semibold">{params.responseTime}</h3>
         <div className="flex items-center gap-3">
           <Clock className="h-5 w-5 text-muted-foreground text-[var(--ring)]" />
-          <span className="text-muted-foreground">{params.responseTimeText}</span>
+          <span className="text-muted-foreground">
+            {params.responseTimeText}
+          </span>
         </div>
       </div>
     </div>
